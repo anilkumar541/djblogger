38aa6865987cb4c0ccf73ff78ba62f123d16ec38
Anil kumar <59431789+anilkumar541@users.noreply.github.com>
2023-01-25 17:36:28 +0530

    okk

--- settings/settings.py
+++ settings/settings.py
@@ -126,6 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "root")
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR, "media")
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
